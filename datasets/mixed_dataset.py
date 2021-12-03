40e9ec54a457d12da457f24f0f5399ef2207beca
Lewis <77823566+hyeonLewis@users.noreply.github.com>
2021-12-03 14:39:46 +0900

    Update mixed_dataset.py

--- datasets/mixed_dataset.py
+++ datasets/mixed_dataset.py
@@ -9,8 +9,8 @@ from .base_dataset import BaseDataset
 class MixedDataset(torch.utils.data.Dataset):
 
     def __init__(self, options, **kwargs):
-        self.dataset_list = ['h36m', 'lsp-orig', 'mpii', 'lspet', 'coco', 'mpi-inf-3dhp']
-        self.dataset_dict = {'h36m': 0, 'lsp-orig': 1, 'mpii': 2, 'lspet': 3, 'coco': 4, 'mpi-inf-3dhp': 5}
+        self.dataset_list = ['h36m', 'lsp-orig', 'mpii', 'lspet', 'coco', 'mpi-inf-3dhp'] # ['lsp-orig', 'mpii', 'lspet', 'coco', 'mpi-inf-3dhp'] 
+        self.dataset_dict = {'h36m': 0, 'lsp-orig': 1, 'mpii': 2, 'lspet': 3, 'coco': 4, 'mpi-inf-3dhp': 5} # {'lsp-orig': 0, 'mpii': 1, 'lspet': 2, 'coco': 3, 'mpi-inf-3dhp': 4} 
         self.datasets = [BaseDataset(options, ds, **kwargs) for ds in self.dataset_list]
         total_length = sum([len(ds) for ds in self.datasets])
         length_itw = sum([len(ds) for ds in self.datasets[1:-1]])
@@ -18,6 +18,7 @@ class MixedDataset(torch.utils.data.Dataset):
         """
         Data distribution inside each batch:
         30% H36M - 60% ITW - 10% MPI-INF
+        If you don't want to use h36m dataset, you need to modify below partition
         """
         self.partition = [.3, .6*len(self.datasets[1])/length_itw,
                           .6*len(self.datasets[2])/length_itw,
